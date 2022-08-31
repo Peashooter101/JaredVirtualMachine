b31ab068d6db1ed0b28cebf465bd38ba12f5f3f8
Peashooter101 <Peashooter101@yahoo.com>
2022-08-31 01:25:49 -0700

    Asset related stuff.
    The Valorant Content from the API does not give me account borders. Sad...

--- src/main/java/com/Peashooter101/experimentation/OverlayImagesExp.java
+++ src/main/java/com/Peashooter101/experimentation/OverlayImagesExp.java
@@ -5,16 +5,60 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class OverlayImagesExp {
 
+    public static final String expImagesPath = "JaredVM_data/Experimentation/Images/";
+
     public static final String valorantGoldIconURI = "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/14/largeicon.png";
     public static final String valorantOmenWideBannerURI = "https://media.valorant-api.com/playercards/7b240a91-4925-8bb6-7812-60b49543e145/wideart.png";
 
-    public static void main(String[] args) {
+    public static final String valorantBorder = "L1T1.png";
+    public static final String valorantOmenSmallURI = "https://media.valorant-api.com/playercards/7b240a91-4925-8bb6-7812-60b49543e145/smallart.png";
 
+    public static void main(String[] args) {
+        // mergeRankAndBanner();
+        generateThumbnail();
+    }
+
+    private static void generateThumbnail() {
+        BufferedImage valorantBorder;
+        BufferedImage omenSmall;
+
+        // Get assets
+        try {
+            valorantBorder = ImageIO.read(new File(expImagesPath + "L1T1.png"));
+            omenSmall = ImageIO.read(new URL(valorantOmenSmallURI));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Merge Image
+        BufferedImage thumbnail = new BufferedImage(valorantBorder.getWidth(), valorantBorder.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        double scaleFactor = (valorantBorder.getWidth() / (double) omenSmall.getWidth()) - .25;
+        int scale = (int) (scaleFactor * omenSmall.getWidth());
+        int offsetX = (thumbnail.getWidth() - scale) / 2;
+        int offsetY = ((thumbnail.getHeight() - scale) / 2) - 7;
+
+        Graphics g = thumbnail.getGraphics();
+        g.drawImage(omenSmall, offsetX, offsetY, scale, scale, null);
+        g.drawImage(valorantBorder, 0, 0, null);
+        g.dispose();
+
+        // Output Image
+        try {
+            ImageIO.write(thumbnail, "PNG", new File(expImagesPath + "thumbnail.png"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void mergeRankAndBanner() {
         BufferedImage goldIcon;
         BufferedImage omenWide;
 
@@ -42,14 +86,13 @@ public class OverlayImagesExp {
 
         // Output Image
         try {
-            ImageIO.write(merged, "PNG", new File("JaredVM_data/Experimentation/Images/merged.png"));
+            ImageIO.write(merged, "PNG", new File(expImagesPath + "merged.png"));
         }
         catch (IOException e) {
             e.printStackTrace();
         }
 
         // TODO: Send to Imgur?: https://apidocs.imgur.com/
-
     }
 
 }
